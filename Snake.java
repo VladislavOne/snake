2b3576c3ccf2db746020bb3f43f2dffbfa407075
Vladislav <shchupakvladislav@gmail.com>
2023-06-15 18:59:35 +0300

    creating a snake body

--- Snake.java
+++ Snake.java
@@ -1,13 +1,26 @@
 package com.javarush.games.snake;
 
+import com.javarush.engine.cell.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Snake {
+    private static final String HEAD_SIGN = "\uD83D\uDC7E";
+    private static final String BODY_SIGN ="\u26AB";
+
     public Snake(int x, int y){
         snakeParts.add(new GameObject(x,y));
         snakeParts.add(new GameObject(x+1,y));
         snakeParts.add(new GameObject(x+2,y));
     }
-    private List<GameObject> snakeParts = new ArrayList<>();
+    private final List<GameObject> snakeParts = new ArrayList<>();
+
+    public void draw(Game game){
+        for (int i = 0; i < snakeParts.size(); i++) {
+            GameObject part = snakeParts.get(i);
+            String sign = (i != 0) ? BODY_SIGN : HEAD_SIGN;
+            game.setCellValue(part.x, part.y, sign);
+        }
+    }
 }
