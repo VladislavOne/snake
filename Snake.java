340cc91be890deb2ddfc3b8e04edec62dbd00b79
Vladislav <shchupakvladislav@gmail.com>
2023-06-15 16:09:17 +0300

    creating a snake

---
+++ Snake.java
@@ -0,0 +1,13 @@
+package com.javarush.games.snake;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Snake {
+    public Snake(int x, int y){
+        snakeParts.add(new GameObject(x,y));
+        snakeParts.add(new GameObject(x+1,y));
+        snakeParts.add(new GameObject(x+2,y));
+    }
+    private List<GameObject> snakeParts = new ArrayList<>();
+}
